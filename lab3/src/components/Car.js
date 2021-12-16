5e60296627d9237ed54e529449ce4cef6e7b04a3
Akkhawat <630112418060@bru.ac.th>
2021-12-16 17:10:10 +0700

    reatt

---
+++ lab3/src/components/Car.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const dom = (
+    <>
+      <h1>hello react !!!</h1>
+      <p>My name is NUK</p>
+    </>
+    );
+
+    function Car(){
+        return<>{dom}</>;
+}
+export default Car
---
+++ lab3/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import KAK from "./components/Car";
+
+
+ReactDOM.render(
+  <>
+  <KAK />
+  </>,
+document.getElementById('root')
+);
+
